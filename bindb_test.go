7bff857f517f953cd5d61a64fd6816e9b56e46af
Kazuki Nishiura <kazuki@sardine.ai>
2021-08-20 11:52:38 -0700

    Update test

--- bindb_test.go
+++ bindb_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func TestBINDB(t *testing.T) {
-	db, error := LoadDB("/Users/hegong/Downloads/iindb3/main.txt", FixLine)
+	db, error := LoadDB("./fixtures/test_data.txt", FixLine)
 	assert.Equal(t, nil, error)
-	assert.Equal(t, "KHAN BANK", db.Map["499999"].Bank)
+	assert.Equal(t, "VISA", db.Map["477938"].Brand)
 }
