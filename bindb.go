2bebb175171534895e52428aa260ff728a3c7c61
hegong <he@Sardine.ai>
2021-09-08 08:41:58 -0700

    add loadmulti

--- bindb.go
+++ bindb.go
@@ -28,6 +28,23 @@ type DB struct {
 	Map map[string]*Record
 }
 
+func InitBindb(path string, autofix func(string) string) (*DB, error) {
+	var err error
+
+	if path[len(path)-1] != '/' {
+		path = path + "/"
+	}
+	db, err := LoadDB(path+"main.txt", autofix)
+	if err != nil {
+		return nil, err
+	}
+	err = LoadMulti(db, path+"multi.txt", autofix)
+	if err != nil {
+		return nil, err
+	}
+	return db, err
+}
+
 func LoadDB(dbpath string, autofix func(string) string) (*DB, error) {
 	var f *os.File
 	var db = &DB{}
@@ -68,6 +85,81 @@ func LoadDB(dbpath string, autofix func(string) string) (*DB, error) {
 	}
 	_ = f.Close()
 	return db, err
+}
+
+func LoadMulti(db *DB, path string, autofix func(string) string) (err error) {
+	var f *os.File
+	f, err = os.Open(path)
+	if err != nil {
+		return err
+	}
+	scanner := bufio.NewScanner(f)
+	scanner.Split(bufio.ScanLines)
+	var line string
+	var fields []string
+	for scanner.Scan() {
+		line = scanner.Text()
+		if autofix != nil {
+			line = autofix(line)
+		}
+		fields = strings.Split(line, "\t")
+		_, err = strconv.ParseInt(fields[0], 10, 32)
+		if err != nil || len(fields) < 12 {
+			fmt.Printf("BINDB row is not valid: %s, fields number is %d\n", line, len(fields))
+			continue
+		}
+		if item, exist := db.Map[fields[0]]; exist {
+			if item.Brand != strings.ToUpper(fields[1]) {
+				item.Brand = ""
+			}
+			if item.Bank != strings.ToUpper(fields[2]) {
+				item.Bank = ""
+			}
+			if item.Type != strings.ToUpper(fields[3]) {
+				item.Type = ""
+			}
+			if item.Level != strings.ToUpper(fields[4]) {
+				item.Level = ""
+			}
+			if item.Info != fields[5] {
+				item.Info = ""
+			}
+			if item.Country != strings.ToUpper(fields[6]) {
+				item.Country = ""
+			}
+			if item.WWW != fields[7] {
+				item.WWW = ""
+			}
+			if item.Phone != fields[8] {
+				item.Phone = ""
+			}
+			if item.Address != fields[9] {
+				item.Address = ""
+			}
+			if item.City != strcase.ToCamel(fields[10]) {
+				item.City = ""
+			}
+			if item.Zip != fields[11] {
+				item.Zip = ""
+			}
+		} else {
+			db.Map[fields[0]] = &Record{
+				Brand:   strings.ToUpper(fields[1]),
+				Bank:    strings.ToUpper(fields[2]),
+				Type:    strings.ToUpper(fields[3]),
+				Level:   strings.ToUpper(fields[4]),
+				Info:    fields[5],
+				Country: strings.ToUpper(fields[6]),
+				WWW:     fields[7],
+				Phone:   fields[8],
+				Address: fields[9],
+				City:    strcase.ToCamel(fields[10]),
+				Zip:     fields[11],
+			}
+		}
+	}
+	_ = f.Close()
+	return nil
 }
 
 func Find(db *DB, bin string) (bindbRecord *Record, err error) {
